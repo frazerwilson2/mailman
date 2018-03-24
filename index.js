053b549a34eca771283e67ef26454f15332281e6
frazer wilson <frazerwilson145@gmail.com>
2018-03-24 10:17:30 +0000

    heroku config

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-server.listen(process.env.PORT || 3000);
+var port = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-console.log('Running');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
